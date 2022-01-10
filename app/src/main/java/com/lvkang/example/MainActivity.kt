17e80da72584a4e88c311b599b7970b52c295b17
345 <lv345_y@163.com>
2022-01-10 22:17:15 +0800

    text ci

--- app/src/main/java/com/lvkang/example/MainActivity.kt
+++ app/src/main/java/com/lvkang/example/MainActivity.kt
@@ -22,10 +22,10 @@ class MainActivity : AppCompatActivity() {
         view.setOnClickListener {
             SkinManager.loadAssetsSkin("skin.skin")
         }
-        findViewById<View>(R.id.button1).setOnClickListener {
-            SkinManager.loadAssetsSkin("skin1.skin")
-
-        }
+//        findViewById<View>(R.id.button1).setOnClickListener {
+//            SkinManager.loadAssetsSkin("skin1.skin")
+//
+//        }
 
         findViewById<View>(R.id.button2).setOnClickListener {
             SkinManager.loadAssetsSkin("skin2.skin")
@@ -84,10 +84,10 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
             null
         }
-        findViewById<View>(R.id.cardview).setOnClickListener {
-            SkinManager.loadAssetsSkin("skin7.skin")
-        }
-
+//        findViewById<View>(R.id.cardview).setOnClickListener {
+//            SkinManager.loadAssetsSkin("skin7.skin")
+//        }
+    }
 
 
 }
